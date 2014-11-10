6a9bba58892552141f5ac71878653c38a0458874
Gavin Brebner <gavin_brebner@hp.com>
2014-11-10 13:50:38 +0100

    Auto-created setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='maze',
+    version='1.0',
+    packages=['maze'],
+    url='',
+    license='',
+    author='gavin',
+    author_email='',
+    description=''
+)
